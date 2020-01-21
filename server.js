4d5e1dd13d81260097e2586175845f5797f060cd
Max McCord <maxdmccord@gmail.com>
2020-01-20 21:31:32 -0600

    added questions in comments of server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -15,15 +16,20 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.post("/submit", ({ body }, res) => {
-  User.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+
+
+// how do I seed the database
+// is the collection (table) name excercises?
+// where should the routes be written?
+// the db is named workout, correct?
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
