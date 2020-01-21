d4ddcea128a214330307ecb4d125123affb8bc0b
Max McCord <maxdmccord@gmail.com>
2020-01-21 14:27:08 -0600

    Jackson fixed bug in public/index.js file

--- public/index.js
+++ public/index.js
@@ -19,10 +19,23 @@ let shouldNavigateAway = false;
 
 init();
 
+// Jackson fixed bug
 async function init() {
+  if (location.pathname.includes("/exercise") && location.search.split("=")[1] === undefined) {
+    console.log("excersise")
+    const newWorkout = await API.createWorkout();
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    }
+    else {
+      newWorkout.classList.add("")
+    }
+    return console.log(newWorkout);
+  }
   if (location.search.split("=")[1] === undefined) {
     const workout = await API.getLastWorkout();
-    if(workout) {
+    if (workout) {
       location.search = "?id=" + workout._id;
     }
     else {
